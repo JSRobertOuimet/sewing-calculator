20a1320119b912da9f9e7d1040c64ada9ce763ca
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-04-14 22:52:23 -0400

    Digits are displayed.

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 (function(window) {
 
-  console.log('Script loaded successfully.');
+  const inputs = document.querySelectorAll('input');
+
+  inputs.forEach(input => {
+    input.addEventListener('click', action);
+  });
+
+  function action() {
+    const inputVal = this.value,
+          re = /\d/g,
+          result = re.test(inputVal);
+
+    if(result === true) {
+      // convert to number and store in array
+      displayNumber(inputVal);
+    }
+    else {
+      console.log('Operation or dot.');
+    }
+  }
+
+  function displayNumber(inputVal) {
+    const display = document.querySelector('#display');
+    display.innerHTML = inputVal;
+  }
 
 })(this);
