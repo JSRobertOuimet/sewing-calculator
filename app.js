e188f7310306d7ea19c63dbaf9e6dd229e5070ac
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-04-15 13:49:22 -0400

    Digits are parsed and stored.

--- app.js
+++ app.js
@@ -1,28 +1,106 @@
-(function(window) {
+(glob => {
 
-  const inputs = document.querySelectorAll('input');
+  const Data = (() => {
+    
+    let instance;
+    let digits = [];
 
-  inputs.forEach(input => {
-    input.addEventListener('click', action);
-  });
+    
+    return () => {
+      if(!instance) {
+        instance = {
+          storeNumbers,
+          joinNumbers
+        };
+      }
+      
+      function storeNumbers(inputVal) {
+        digits.push(inputVal);
 
-  function action() {
-    const inputVal = this.value,
-          re = /\d/g,
-          result = re.test(inputVal);
+        return digits;
+      }
+      
+      function joinNumbers(digits) {
+        let joinedDigits;
+        
+        joinedDigits = digits.join('');
 
-    if(result === true) {
-      // convert to number and store in array
-      displayNumber(inputVal);
-    }
-    else {
-      console.log('Operation or dot.');
-    }
-  }
+        return joinedDigits;
+      }
 
-  function displayNumber(inputVal) {
-    const display = document.querySelector('#display');
-    display.innerHTML = inputVal;
-  }
+      return instance;
+    };
+
+  })();
+
+  const Operations = (() => {
+
+    let instance;
+
+    return () => {
+      if(!instance) {
+        instance = {
+          action
+        };
+      }
+
+      function action() {
+        const data = Data(),
+              ui = UI();
+
+        const inputVal = this.value,
+              re = /\d|\./g,
+              result = re.test(inputVal);
+    
+        if(result) {
+          let digits,
+              parsedNumbers;
+
+          digits = data.storeNumbers(inputVal);
+          parsedNumbers = parseFloat(data.joinNumbers(digits), 10);
+          
+          ui.displayNumber(parsedNumbers);
+        }
+        else {
+          console.log('Operation or dot.');
+        }
+      }
+
+      return instance;
+    };
+
+  })();
+
+  const UI = (() => {
+
+    let instance;
+
+    return () => {
+      if(!instance) {
+        instance = {
+          displayNumber
+        };
+      }
+
+      function displayNumber(inputVal) {
+        const display = document.querySelector('#display');
+        display.innerHTML = inputVal;
+      }
+
+      return instance;
+    };
+
+  })();
+
+  const App = ((Operation) => {
+
+    const op = Operations(),
+          inputs = document.querySelectorAll('input');
+
+    inputs.forEach(input => {
+      input.addEventListener('click', op.action);
+    });
+
+  })(Operations);
 
 })(this);
